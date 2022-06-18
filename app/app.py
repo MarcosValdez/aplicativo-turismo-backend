a42a29710a8643334483b5ba941e1955039f310c
EOrtega27 <ortegachaveznsm@gmail.com>
2022-06-18 10:34:32 -0500

    basic firestore connection

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db=firestore.client()
 app = Flask(__name__)
 
 @app.route("/")
