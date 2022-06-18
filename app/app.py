bd98c6893df1a018a29b3f8f2fc41e08e892d29e
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-06-18 11:34:43 -0500

    fix: Import library

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-from biblioteca import biblioteca
+from biblioteca.biblioteca import biblioteca
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -10,15 +10,15 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 app = Flask(__name__)
 
-bibClass = biblioteca
-
 @app.route("/")
 def home():
     return render_template('home.html')
 
 @app.route('/biblioteca', methods=['GET'])
 def test():
-    bibClass.test(db)
+    bibClass = biblioteca()
+    resp = bibClass.test(db)
+    return jsonify({'msg': resp})
 
 @app.route("/login")
 def login():
@@ -27,8 +27,6 @@ def login():
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port = 4000)
--- app/biblioteca/biblioteca.py
+++ app/biblioteca/biblioteca.py
@@ -3,14 +3,18 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 app = Flask(__name__)
 
-class bilioteca:
+class biblioteca():
     def test(self, db):
-        db.collection('traduccion').add({
-            'email': 'ortegachavez@gmail.com',
-            'idm_origen': 'eng',
-            'idm_traduc': 'esp',
-            'txt_origen': 'Hello',
-            'txt_traduc': 'Hola',
-            'imagen': 'link'
-        })
-        return "Traduccion insertada"
+        try:
+            db.collection('traduccion').add({
+                'email': 'ortegachavez@gmail.com',
+                'idm_origen': 'eng',
+                'idm_traduc': 'esp',
+                'txt_origen': 'Hello',
+                'txt_traduc': 'Hola',
+                'imagen': 'link'
+            })
+        except Exception as e: 
+            print(f'Exception: {e}')
+
+        return "Traducción insertada"
