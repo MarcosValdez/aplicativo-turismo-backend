cdd9917f28f15dcf78bd1d53755ea1de753e3fc1
EOrtega27 <ortegachaveznsm@gmail.com>
2022-06-18 10:48:57 -0500

    Firestore connection

--- app/app.py
+++ app/app.py
@@ -2,12 +2,24 @@ from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+from biblioteca import biblioteca
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-db=firestore.client()
+db = firestore.client()
 app = Flask(__name__)
+
+bibClass = biblioteca
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/biblioteca', methods=['GET'])
+def test():
+    biblioteca.test(db)
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 4000)
+
