b484f454f543f97d16179e9f90a68a9c301f5918
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-06-23 11:11:06 -0500

    chore: change path

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from firebase_admin import credentials, firestore
 import firebase_admin
 
-cred = credentials.Certificate("./app/serviceAccountKey.json")
+cred = credentials.Certificate("./src/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
