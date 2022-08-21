db35c57cff5c783a0897b3e773ba208e528e3203
MarcosValdez <marcos.valdez333@hotmail.com>
2022-08-20 23:50:42 -0500

    feat: conteo de palabras

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from .routes.conteo import conteo
 from .routes.biblioteca import biblioteca_routes
 from .routes.views import views
 from .routes.task import task
+
 from flask_cors import CORS
 
 
@@ -12,6 +14,7 @@ def create_app():
 
     app.register_blueprint(biblioteca_routes, url_prefix='/biblioteca')
     app.register_blueprint(task, url_prefix='/task')
+    app.register_blueprint(conteo, url_prefix='/conteo')
     app.register_blueprint(views)
 
     return app
---
+++ src/routes/conteo.py
@@ -0,0 +1,69 @@
+from src.utils.db import connect_database
+from flask import Blueprint, jsonify, request
+from flask_cors import cross_origin
+
+conteo = Blueprint('conteo', __name__)
+db = connect_database()
+
+@cross_origin
+@conteo.route("/contar_ingles/<string:sentence>")
+def contar_palabras_ingles(sentence):
+    try:
+        diccionario_frecuencias = contar(sentence)
+        for palabra in diccionario_frecuencias:
+            nuevo = {
+                'palabra': palabra,
+                'frecuencia': diccionario_frecuencias[palabra]
+            }
+            conteo_ref = db.collection('conteo_ingles').document(nuevo['palabra']).get()
+            if conteo_ref.exists:
+                frecuencia_nueva = conteo_ref.to_dict()["frecuencia"] + nuevo['frecuencia']
+                db.collection('conteo_ingles').document(nuevo['palabra']).update({
+                    'palabra': nuevo['palabra'],
+                    'frecuencia': frecuencia_nueva
+                })
+            else:
+                db.collection('conteo_ingles').document(nuevo['palabra']).set(nuevo)
+        return jsonify("Exito"), 200
+    except Exception as e:
+        return jsonify({
+            'error': f'Exception: {e}'
+        }), 400
+
+@cross_origin
+@conteo.route("/contar_espanol/<string:sentence>")
+def contar_palabras_espanol(sentence):
+    
+    diccionario_frecuencias = contar(sentence)
+
+    for palabra in diccionario_frecuencias:
+        nuevo = {
+            'palabra': palabra,
+            'frecuencia': diccionario_frecuencias[palabra]
+        }
+        conteo_ref = db.collection('conteo_espanol').document(nuevo['palabra']).get()
+        if conteo_ref.exists:
+            frecuencia_nueva = conteo_ref.to_dict()["frecuencia"] + nuevo['frecuencia']
+            db.collection('conteo_espanol').document(nuevo['palabra']).update({
+                'palabra': nuevo['palabra'],
+                'frecuencia': frecuencia_nueva
+            })
+        else:
+            db.collection('conteo_espanol').document(nuevo['palabra']).set(nuevo)
+    return jsonify("Exito"), 200
+
+def contar(sentence):
+    quitar = ".,;:\n!\"'"
+    for caracter in quitar:
+        sentence = sentence.replace(caracter,"")
+
+    sentence = sentence.lower()
+    palabras = sentence.split(" ")
+
+    diccionario_frecuencias = {}
+    for palabra in palabras:
+        if palabra in diccionario_frecuencias:
+            diccionario_frecuencias[palabra] += 1
+        else:
+            diccionario_frecuencias[palabra] = 1
+    return diccionario_frecuencias
